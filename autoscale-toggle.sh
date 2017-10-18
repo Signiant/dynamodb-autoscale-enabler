3e5f7f0418deb9434c29ebfcbb27e89e1d6db08a
Dave North <dnorth@signiant.com>
2017-10-18 09:40:42 -0400

    Check for the minimum version of the AWS cli required that supports application autoscaling

--- autoscale-toggle.sh
+++ autoscale-toggle.sh
@@ -15,6 +15,55 @@ usage()
   exit 1
 }
 
+check_cli_version()
+{
+  min_ver='1.11.129'
+  current_ver=$(aws --version 2>&1 |cut -f2 -d"/" |cut -f1 -d" ")
+
+  vercomp ${current_ver} ${min_ver}
+  status=$?
+
+  if [ ${status} -eq 2 ]; then
+    echo "You do not have the required version of the AWS CLI to run this script"
+    echo "Your version: ${current_ver}  Minimum version: ${min_ver}"
+    echo "Use: pip install awscli --upgrade"
+    exit 1
+  fi
+}
+
+# Compare 2 symantic versions
+# https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
 # Sees if a role exists in IAM
 role_exists()
 {
@@ -335,6 +384,8 @@ handle_resource()
 # ======================
 # ====== MAIN
 # ======================
+
+check_cli_version
 
 while getopts ":m:r:p:i:x:" o; do
     case "${o}" in
